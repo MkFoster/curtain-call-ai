e2727b1396c17322a0bfb9d3f7166bf5ea9cbc25
Mark Foster <mark@mkfoster.com>
2024-04-29 21:52:01 -0700

    Added animation via Johnny-Five and Arduino.

--- run-show.js
+++ run-show.js
@@ -1,3 +1,4 @@
+const {Board, Servo} = require("johnny-five");
 const dmx = require("enttec-open-dmx-usb");
 const dmxDevice = dmx.EnttecOpenDMXUSBDevice;
 const fs = require("fs").promises;
@@ -5,7 +6,134 @@ const textToSpeech = require("@google-cloud/text-to-speech");
 const { spawn } = require("node:child_process");
 const path = require("path");
 
-let device;
+const boardComPort = "COM6";
+
+
+// Run the show
+(async () => {
+    let dmx;
+    let board;
+    let character1Servo;
+    let character2Servo;
+
+    let lightingEnabled = true;
+    try {
+        dmx = new dmxDevice(await dmxDevice.getFirstAvailableDevice());
+    } catch (error) {
+        console.log(
+            "No DMX interface found.  Proceeding without lighting.  Error: ",
+            error
+        );
+        lightingEnabled = false;
+    }
+
+    let boardEnabled = true;
+    try {
+        board = await getBoard(boardComPort);
+        character1Servo = new Servo({
+            pin: 10,
+            range: [ 10, 170 ],
+            startAt: 90
+        });
+    
+        character2Servo = new Servo({
+            pin: 11,
+            range: [ 10, 170 ],
+            startAt: 90
+        });
+    } catch (error) {
+        console.log(
+        "No Board found.  Proceeding without animations.  Error: ",
+        error
+        );
+        boardEnabled = false;
+    }
+
+    const showFolder = "shows" + path.sep;
+    const soundFolder = "sound-effects" + path.sep;
+    const rawJSON = await fs.readFile(showFolder + "script.json", "utf8");
+    const show = JSON.parse(rawJSON);
+
+    for (let i = 0; i < show.cues.length; i++) {
+        const cueObj = show.cues[i];
+        switch (cueObj.type) {
+            case "lights":
+                if (lightingEnabled) {
+                    dmx.setChannels(cueObj.channelValues);
+                }
+                break;
+            case "wait":
+                await sleep(cueObj.duration);
+                break;
+            case "script":
+                await playMP3(showFolder + cueObj.audioFile);
+                break;
+            case "soundEffect":
+                await playMP3(soundFolder + cueObj.audioFile);
+                break;
+            case "background":
+                /*await displayImage(
+                    __dirname + path.sep + showFolder + cueObj.bgImageFile
+                );*/
+                break;
+            case "animate":
+                if (boardEnabled) {
+                    let servo;
+                    if (cueObj.character == "character1") {
+                        servo = character1Servo;
+                    } else if (cueObj.character == "character2") {
+                        servo = character2Servo;
+                    } else {
+                        console.log("Unknown character animation.");
+                    }
+                    switch (cueObj.motion) {
+                        case "faceForward":
+                            servo.center();
+                            break;
+                        case "faceFullRight":
+                            servo.min();
+                            break;
+                        case "faceFullLeft":
+                            servo.max();
+                            break;
+                        case "facePartRight":
+                            servo.to(50);
+                            break;
+                        case "facePartLeft":
+                            servo.to(130);
+                            break;
+                        default:
+                            console.log("Unknown motion.");
+                    }
+                }
+                break;
+            default:
+                console.error(`Unknown cue type: ${cueObj.type}`);
+                break;
+        }
+    }
+    process.exit();
+})();
+
+async function sleep(durationMilliseconds) {
+    await new Promise((resolve) =>
+        setTimeout(resolve, durationMilliseconds)
+    );
+}
+
+async function getBoard(arduinoComPort) {
+    return new Promise((resolve, reject) => {
+      const board = new Board({ port: arduinoComPort });
+      board.on("ready", function () {
+        // Board is ready, resolve the promise with the board object
+        resolve(board);
+      });
+      board.on("error", function (error) {
+        // Board error, reject the promise with the error
+        reject(error);
+      });
+    });
+}
 
 async function playMP3(mp3Path) {
     try {
@@ -47,52 +175,3 @@ async function displayImage(imagePath) {
     }
 }
 
-// Run the show
-(async () => {
-    let lightingEnabled = true;
-    try {
-        device = new dmxDevice(await dmxDevice.getFirstAvailableDevice());
-    } catch (error) {
-        console.log(
-            "No DMX interface found.  Proceeding without lighting.  Error: ",
-            error
-        );
-        lightingEnabled = false;
-    }
-
-    const showFolder = "shows" + path.sep;
-    const soundFolder = "sound-effects" + path.sep;
-    const rawJSON = await fs.readFile(showFolder + "script.json", "utf8");
-    const show = JSON.parse(rawJSON);
-
-    for (let i = 0; i < show.cues.length; i++) {
-        const cueObj = show.cues[i];
-        switch (cueObj.type) {
-            case "lights":
-                if (lightingEnabled) {
-                    device.setChannels(cueObj.channelValues);
-                }
-                break;
-            case "wait":
-                await new Promise((resolve) =>
-                    setTimeout(resolve, cueObj.duration)
-                );
-                break;
-            case "script":
-                await playMP3(showFolder + cueObj.audioFile);
-                break;
-            case "soundEffect":
-                await playMP3(soundFolder + cueObj.audioFile);
-                break;
-            case "background":
-                await displayImage(
-                    __dirname + path.sep + showFolder + cueObj.bgImageFile
-                );
-                break;
-            default:
-                console.error(`Unknown cue type: ${cueObj.type}`);
-                break;
-        }
-    }
-    process.exit();
-})();
